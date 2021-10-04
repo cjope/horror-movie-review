9f3858b782ebceb815bb6dc93350163eaf503433
Craig <craigjope@gmail.com>
2021-10-04 01:29:08 -0600

    fixed rating from 1/10 to 1/5

--- src/VotetoStars.js
+++ src/VotetoStars.js
@@ -3,11 +3,11 @@ function VoteToStars({review}){
     const p1 = <img className="pumpkin-light" alt="pumpkin-light" src="https://d29fhpw069ctt2.cloudfront.net/icon/image/85188/preview.svg"></img>
     const p0 = <img className="pumpkin-dark" alt="pumpkin-dark" src="https://d29fhpw069ctt2.cloudfront.net/icon/image/85188/preview.svg"></img>
 
-    if (voteAverage <3) return <>{p1}{p0}{p0}{p0}{p0}</>
-    if (voteAverage >=3 && voteAverage <5) return <>{p1}{p1}{p0}{p0}{p0}</>
-    if (voteAverage >=5 && voteAverage <7) return <>{p1}{p1}{p1}{p0}{p0}</>
-    if (voteAverage >=7 && voteAverage <9) return <>{p1}{p1}{p1}{p1}{p0}</>
-    if (voteAverage >=9) return <>{p1}{p1}{p1}{p1}{p1}</>
+    if (voteAverage ===1) return <>{p1}{p0}{p0}{p0}{p0}</>
+    if (voteAverage ===2) return <>{p1}{p1}{p0}{p0}{p0}</>
+    if (voteAverage ===3) return <>{p1}{p1}{p1}{p0}{p0}</>
+    if (voteAverage ===4) return <>{p1}{p1}{p1}{p1}{p0}</>
+    if (voteAverage ===5) return <>{p1}{p1}{p1}{p1}{p1}</>
     else return <>{p0}{p0}{p0}{p0}{p0}</>
     }    
 export default VoteToStars
