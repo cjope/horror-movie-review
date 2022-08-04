dbc4ba5baa8cd4bfc23982914645b804d8c32a19
Craig <craigjope@gmail.com>
2022-08-04 12:32:47 -0600

    mobile friendly

--- src/Test.js
+++ src/Test.js
@@ -6,6 +6,7 @@ import MovieCard from "./MovieCard";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Reviews from "./Reviews";
+import { BrowserView, MobileView } from "react-device-detect";
 
 
 function Test(){
@@ -19,6 +20,7 @@ function Test(){
     const browseURL = `https://api.themoviedb.org/3/discover/${fakeRoute}?api_key=058b20ba9bda19035670479e41a673af`
     const searchURL = `https://api.themoviedb.org/3/search/${fakeRoute}?api_key=058b20ba9bda19035670479e41a673af&sort_by=popularity.desc&query=`
     const getGenres = `https://api.themoviedb.org/3/genre/${fakeRoute}/list?api_key=058b20ba9bda19035670479e41a673af&language=en-US`
+    
 
     useEffect(()=>{
         fetch(getGenres)
@@ -72,7 +74,7 @@ function Test(){
 
     return(
         <div style={{display:"flex",flexDirection:"column", height:"100vh", backgroundColor:"rgb(25, 25, 25)"}}>
-            <div style={{flex:3, display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <BrowserView style={{flex:3, display:"flex", justifyContent:"center", alignItems:"center"}}>
                 <ToggleButtonGroup
                     color="primary"
                     value={fakeRoute}
@@ -84,9 +86,11 @@ function Test(){
                     <ToggleButton color="warning" sx={{flex:1, color:"white"}}  value="movie">MOVIES</ToggleButton>
                     <ToggleButton color="warning" sx={{flex:1, color:"white"}}  value="tv">SHOWS</ToggleButton>
                 </ToggleButtonGroup>
+                {fakeRoute !== ""?
                 <div style={{flex:2}}>
                     <TextField  disabled={fakeRoute === ""}  label="SEARCH" color="warning" sx={{ input: { color: 'orange' } }} variant="outlined" onChange={handleSearch} style={{ width:"90%"}} />
-                </div> 
+                </div>:null}
+                {fakeRoute !== ""?
                 <div style={{flex:1}}>
                     <FormControl disabled={fakeRoute === ""}  style={{ marginLeft:"2%", width:"90%"}}>
                         <InputLabel id="select-label" style={{color:"orange"}}>FILTER</InputLabel>
@@ -95,7 +99,8 @@ function Test(){
                                 {genreData.map(genre=><MenuItem style={{color:"white"}} value={genre.id}>{genre.name}</MenuItem>)}
                              </Select>
                     </FormControl>
-                </div>
+                </div>:null}
+                {fakeRoute !== ""?
                 <div style={{flex:1}}>
                     <FormControl disabled={fakeRoute === ""}  style={{ marginLeft:"2%",width:"90%"}}>
                         <InputLabel id="select-label" style={{color:"orange"}}>SORT</InputLabel>
@@ -105,12 +110,57 @@ function Test(){
                                <MenuItem style={{color:"white"}} value="original_title.desc">Title</MenuItem>
                             </Select>
                     </FormControl>
-                </div>
+                </div>:null}
                 <div style={{flex:1, display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
                     <h1 style={{color:"orange", flex:5, margin:"unset"}}>ADD TO </h1>
                     <img style={{maxHeight:"45px", flex:1}} src="https://www.plex.tv/wp-content/themes/plex/assets/img/plex-logo.svg" alt="plex-logo" ></img>
                 </div>
-            </div>
+            </BrowserView>
+
+            <MobileView>
+                <div style={{display:"flex"}}>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={fakeRoute}
+                    exclusive
+                    onChange={e=>setFakeRoute(e.target.value)}
+                    sx={{flex:2}}
+                >
+                    <ToggleButton color="warning" sx={{flex:1, color:"white"}} value="">QUEUE</ToggleButton>
+                    <ToggleButton color="warning" sx={{flex:1, color:"white"}}  value="movie">MOVIES</ToggleButton>
+                    <ToggleButton color="warning" sx={{flex:1, color:"white"}}  value="tv">SHOWS</ToggleButton>
+                </ToggleButtonGroup>
+                <div style={{flex:1, display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+                    <h1 style={{color:"orange", flex:5, margin:"unset"}}>ADD TO </h1>
+                    <img style={{maxHeight:"45px", flex:1}} src="https://www.plex.tv/wp-content/themes/plex/assets/img/plex-logo.svg" alt="plex-logo" ></img>
+                </div>
+                </div>
+                {fakeRoute !== ""?
+                <div>
+                    <TextField  disabled={fakeRoute === ""}  label="SEARCH" color="warning" sx={{ input: { color: 'orange' } }} variant="outlined" onChange={handleSearch} style={{ width:"90%"}} />
+                </div> :null}
+                {fakeRoute !== ""?
+                <div style={{flex:1}}>
+                    <FormControl disabled={fakeRoute === ""}  style={{ marginLeft:"2%", width:"90%"}}>
+                        <InputLabel id="select-label" style={{color:"orange"}}>FILTER</InputLabel>
+                            <Select defaultValue={""} style={{color:"white"}} onChange={handleGenre}>
+                                <MenuItem style={{color:"white"}} value="">All</MenuItem>
+                                {genreData.map(genre=><MenuItem style={{color:"white"}} value={genre.id}>{genre.name}</MenuItem>)}
+                             </Select>
+                    </FormControl>
+                </div>:null}
+                {fakeRoute !== ""?
+                <div style={{flex:1}}>
+                    <FormControl disabled={fakeRoute === ""}  style={{ marginLeft:"2%",width:"90%"}}>
+                        <InputLabel id="select-label" style={{color:"orange"}}>SORT</InputLabel>
+                            <Select defaultValue={""} style={{color:"white"}} onChange={handleSort}>
+                               <MenuItem style={{color:"white"}} value="popularity.desc">Popular</MenuItem> 
+                               <MenuItem style={{color:"white"}} value="release_date.desc">Release</MenuItem> 
+                               <MenuItem style={{color:"white"}} value="original_title.desc">Title</MenuItem>
+                            </Select>
+                    </FormControl>
+                </div>:null}
+            </MobileView>
             {
             fakeRoute !== "" ?
                 <div style={{flex:15, display:"flex"   }}>
